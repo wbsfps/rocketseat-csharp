5de5f97c105de4e598a4857d0fa1a96779196c86
William Santana <williamandrade1058@gmail.com>
2025-05-19 11:17:03 -0300

    aula sobre tipos booleanos

---
+++ Fundamentos_CSharp/Fundamentos_CSharp/tipos_booleanos/TiposBooleanos.cs
@@ -0,0 +1,10 @@
+﻿namespace Fundamentos_CSharp.tipos_booleanos;
+
+public class TiposBooleanos
+{
+    static void Main()
+    {
+        bool active = true;
+        bool inactive = false;
+    }
+}
