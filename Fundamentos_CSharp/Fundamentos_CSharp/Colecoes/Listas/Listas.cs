aa56bfe9f896dee243ee17fed7a1f1544de44203
William Santana <williamandrade1058@gmail.com>
2025-05-20 14:43:01 -0300

    tópico sobre listas

---
+++ Fundamentos_CSharp/Fundamentos_CSharp/Colecoes/Listas/Listas.cs
@@ -0,0 +1,31 @@
+﻿namespace Fundamentos_CSharp.Colecoes.Listas;
+
+public class Listas
+{
+    public static void Run()
+    {
+        List<int> ints = new List<int>();
+        ints.Add(1);
+        ints.Add(2);
+        ints.Add(3);
+
+        Console.WriteLine(ints[0]);
+        Console.WriteLine(ints[1]);
+        // Console.WriteLine(ints[2]); OutRange
+        Console.WriteLine(ints.Count);
+
+        ints.RemoveAt(1); // Remover elemento da posição 0
+        int elementAt = ints.ElementAt(0); // Pegar elemento da posição 0
+
+        int firstElement = ints.First();
+        int lastElement = ints.Last();
+        Console.WriteLine(firstElement);
+        Console.WriteLine(lastElement);
+
+        ints.Remove(1);
+        ints.Remove(2);
+        Console.WriteLine(ints.Count);
+
+        ints.Clear(); // Remover todos os elementos
+    }
+}
---
+++ Fundamentos_CSharp/Fundamentos_CSharp/Colecoes/Listas/Listas2.cs
@@ -0,0 +1,37 @@
+﻿namespace Fundamentos_CSharp.Colecoes.Listas;
+
+public class Listas2
+{
+    public static void Run()
+    {
+        List<string> strings = new List<string>();
+        strings.Add("I");
+        strings.Add("am");
+        strings.Add("from");
+        strings.Add("Brazil");
+
+        strings.Remove("am");
+        
+        foreach (var text in strings)
+        {
+            Console.WriteLine(text);
+        }
+
+        List<int> ints = new List<int>();
+        ints.Add(0);
+        ints.Add(1);
+        List<decimal> decimals = new List<decimal>();
+        List<bool> bools = new List<bool>();
+
+        List<object> obejcts = new List<object>();
+        obejcts.Add("Hello");
+        obejcts.Add(1);
+        obejcts.Add(true);
+
+        string result = string.Join(" ", strings);
+        Console.WriteLine(result);
+
+        result = string.Join("-", ints);
+        Console.WriteLine(result);
+    }
+}
